ec589283bc3604b36878b726c83d41593e344d7b
Gokul G.K <gokulgk1200@gmail.com>
2023-02-18 22:23:16 +0530

    adding list

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import list from './components/List';
+const name = 'Bois';
 
+function getName(){
+  return name;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> ,nthada mwone fear ayo?.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {getName()}</h1>
+      <div>{list()}</div>
     </div>
   );
 }
