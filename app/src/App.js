a89048a35fbb84694fdab1da7d0d8e736978b59f
Gokul G.K <gokulgk1200@gmail.com>
2023-02-19 22:52:30 +0530

    Adding props

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import list from './components/List';
-const name = 'Bois';
 
-function getName(){
-  return name;
-}
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Hello {getName()}</h1>
-      <div>{list()}</div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <hr />
+
+      <List list={stories} />
     </div>
   );
-}
+};
+
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>&nbsp;
+      </span>
+      <span>{item.author}&nbsp;</span>
+      <span>{item.num_comments}&nbsp;</span>
+      <span>{item.points}&nbsp;</span>
+    </div>
+  ));
 
 export default App;
