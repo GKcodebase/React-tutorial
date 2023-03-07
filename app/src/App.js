a2524debb4bd7736aaf191252ac2dc688accec0c
Gokul G.K <gokulgk1200@gmail.com>
2023-03-07 21:36:19 +0530

    Adding svg

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
-import './App.css';
+import  './App.css';
+import { ReactComponent as Check } from './check.svg';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -204,7 +205,7 @@ const Item = ({ item, onRemoveItem }) => (
         onClick={() => onRemoveItem(item)}
         className="button button_small"
       >
-        Dismiss
+        <Check height="18px" width="18px" />
       </button>
     </span>
   </div>
